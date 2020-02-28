f49440f3535e89e65beb58230ab1c9072455fbd5
YescoSoftware <software@yesco.com>
2020-02-28 08:17:16 -0700

    Update imagepicker.js

--- www/imagepicker.js
+++ www/imagepicker.js
@@ -54,14 +54,14 @@ ImagePicker.prototype.getPictures = function(success, fail, options) {
 	this.validateOutputType(options);
 
 	var params = {
-		maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 15,
+		maximumImagesCount: options.maximumImagesCount ? options.maximumImagesCount : 5,
 		width: options.width ? options.width : 0,
 		height: options.height ? options.height : 0,
 		quality: options.quality ? options.quality : 100,
 		allow_video: options.allow_video ? options.allow_video : false,
-		title: options.title ? options.title : 'Select an Album', // the default is the message of the old plugin impl
+		title: options.title ? options.title : 'Select Camera Roll', // the default is the message of the old plugin impl
 		message: options.message ? options.message : null, // the old plugin impl didn't have it, so passing null by default
-		outputType: options.outputType ? options.outputType : this.OutputType.FILE_URI,
+		outputType: options.outputType ? options.outputType : this.OutputType.BASE64_STRING,
 		disable_popover: options.disable_popover ? options.disable_popover : false // Disable the iOS popover as seen on iPad
 	};
 
