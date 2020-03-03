4c41655eec94f75a36bfb7b476c3e08e4b4b84eb
YescoSoftware <software@yesco.com>
2020-03-03 14:21:42 -0700

    Update ImagePicker.java

--- src/android/com/toshyodev/ImagePicker/ImagePicker.java
+++ src/android/com/toshyodev/ImagePicker/ImagePicker.java
@@ -22,6 +22,7 @@ import android.content.pm.PackageManager;
 import android.os.Build;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 public class ImagePicker extends CordovaPlugin {
 
